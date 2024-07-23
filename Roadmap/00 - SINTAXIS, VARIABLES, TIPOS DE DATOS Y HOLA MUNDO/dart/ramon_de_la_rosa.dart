80ce701a27f26f025a095d28ab565f0c84cd9a06
ramon-creator <ramon.delarosa@gmail.com>
2024-07-22 21:34:20 -0400

    00-sintaxis

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/dart/ramon_de_la_rosa.dart
@@ -0,0 +1,53 @@
+// www.dart.dev
+
+/** estoo es un comentario multilinea,
+ * sirve para hacer comentarios de amplio espetro en multiples lineas
+ */
+
+import 'dart:async';
+
+/// este es un comentario de documentacion,
+/// hace la misma funcion de un comentario multilinea
+/// nos sirve para extendeernos en el comentario
+
+//Tipos de variables en dart:
+ 
+// Variable Simple:
+
+var name = 'juan';
+
+//Variable Dinamica
+
+Object name = 'jose';
+
+// Variable Explicita
+
+String name = 'pedro';
+
+//Variable de seguridad nula
+
+String? name = ;
+
+// Variable Final y Variable constante 
+
+final name = 'alberto';
+const name = 'julian';
+
+// Datos primitivos en dart
+
+String name = 'joselito';
+double num = 2.5;
+int num2 = 5;
+bool name = true;
+
+// Impresion por consola de hola lenguaje
+
+String lenguaje = 'Dart';
+
+print ('¡Hola
+, $lenguaje!');
+
+
+
+
+
